1507ced85d8815bfb7550fa2aa88d45ade7ea805
mafernandezz <ma.fernandezz@duocuc.cl>
2025-09-24 21:18:17 -0300

    Actualizacion lambda v3

--- actividades-formativas/actividad-05/hola-aws-lambda.js
+++ actividades-formativas/actividad-05/hola-aws-lambda.js
@@ -2,7 +2,7 @@ export const handler = async (event) => {
     // TODO implement
     const response = {
         statusCode: 200,
-        body: JSON.stringify('Chapa la pachala v2'),
+        body: JSON.stringify('Chapa la pachala v3'),
     };
     return response;
 };
